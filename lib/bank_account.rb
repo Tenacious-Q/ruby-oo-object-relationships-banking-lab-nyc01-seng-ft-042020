e799864130e3e134c5920f1433106d57717c23b5
Tenacious-Q <mikequigley@quigleypublishing.com>
2020-04-23 17:38:53 -0400

    finished except for #execute_transaction and #reverse_transfer

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,34 @@
 class BankAccount
+    attr_accessor :transfer, :balance, :status 
+    attr_reader :name 
+    #attr_writer 
+
+
+    def initialize(name, transfer="pending", status="open", balance=1000)
+        @transfer = transfer 
+        @name = name 
+        @balance = balance 
+        @status = status 
+    end 
+
+    def deposit(amount)
+        @balance += amount 
+    end 
+
+    def display_balance
+        "Your balance is $#{@balance}." 
+    end 
+
+    def valid?
+        status == "open" && @balance > 0 
+    end 
+
+    def close_account
+        #if @balance == 0 
+        #    "closed" 
+        #end 
+        self.status = "closed"
+    end 
 
 end
+
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,32 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+
+  # initialize
+  #   can initialize a Transfer
+  #   initializes with a sender
+  #   initializes with a receiver
+  #   always initializes with a status of 'pending'
+  #   initializes with a transfer amount    (we can tell it should be 50 because of the error)
+  def initialize(sender, receiver, amount=50, status="pending")
+    @sender = sender 
+    @receiver = receiver 
+    @amount = amount
+    @status = status 
+  end 
+
+  # #valid?
+  # can check that both accounts are valid
+  # calls on the sender and receiver's #valid? methods
+  def valid?
+    sender.valid? && receiver.valid? 
+  end 
+
+  def execute_transaction
+  end 
+
+  def reverse_transfer
+  end 
+
+
+
 end
